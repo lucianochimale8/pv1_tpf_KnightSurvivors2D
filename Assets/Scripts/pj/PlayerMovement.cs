4605af66728fde4d4ba6405ba054ec35d3b82920
lucianochimale8 <kchimale@gmail.com>
2025-11-11 07:42:55 -0300

    agregando animaciones al jugador, con su script para manejar, agregue el dia. feature/player-animations

--- Assets/Scripts/pj/PlayerMovement.cs
+++ Assets/Scripts/pj/PlayerMovement.cs
@@ -1,5 +1,9 @@
 using UnityEngine;
 
+/*
+ * Script para el movimiento del personaje
+ */
+
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
@@ -8,13 +12,17 @@ public class PlayerMovement : MonoBehaviour
     private PlayerInput playerInput;
     private SpriteRenderer spriteRenderer;
 
+    // Propiedad publica para saber si se esta moviendo
+    public bool IsMoving { get; private set; }
+
+    // Awake para cargar los componentes cuando se inicia el proyecto
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         playerInput = GetComponent<PlayerInput>();
         spriteRenderer = GetComponent<SpriteRenderer>();    
     }
-
+    // FixedUpdate para controlar las fisicas
     private void FixedUpdate()
     {
         Vector2 input = playerInput.GetMovementInput();
@@ -22,13 +30,20 @@ public class PlayerMovement : MonoBehaviour
         Flip(input.x);
 
     }
-
+    /*
+     * Metodo para mover al personaje 
+     * mediante un parametro que reconoce el input
+     */
     private void Move(Vector2 input)
     {
         Vector2 movement = input.normalized;
         rb.linearVelocity = movement * speed;
+        IsMoving = movement.magnitude > 0.1f;
     }
-
+    /*
+     * Metodo para reconocer para que lado 
+     * esta dirigiendose el personaje para voltear la imagen
+     */
     private void Flip(float horizontal)
     {
         if (horizontal > 0.1f)
